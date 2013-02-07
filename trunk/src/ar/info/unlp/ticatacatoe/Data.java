38cdcec9d8e7fce3e84eb5c8cffad971e4105e91
federicocristina@gmail.com <federicocristina@gmail.com@f82cbd09-2634-8b74-c650-5d28e03e2eee>
2013-02-07 10:52:34 +0000

    Changed from Android only platform to multi-platform (according to NetworkDCQ)

--- trunk/src/ar/info/unlp/ticatacatoe/Data.java
+++ trunk/src/ar/info/unlp/ticatacatoe/Data.java
@@ -1,8 +1,11 @@
 package ar.info.unlp.ticatacatoe;
 
+import networkdcq.Host;
 import networkdcq.NetworkApplicationData;
+import networkdcq.discovery.HostDiscovery;
+import networkdcq.util.NetworkSerializable;
 
-public class Data extends NetworkApplicationData {
+public class Data extends NetworkApplicationData implements NetworkSerializable {
 
 	/**
 	 * 
@@ -22,5 +25,31 @@ public class Data extends NetworkApplicationData {
 	public int action = -1;
 	/** 1 to 9 */
 	public int position = -1;
+	
+	
+	public String networkSerialize() {
+
+		return ( HostDiscovery.thisHost.getHostIP() + NetworkSerializable.VARIABLE_MEMBER_SEPARATOR +
+				 HostDiscovery.thisHost.isOnLine() + NetworkSerializable.VARIABLE_MEMBER_SEPARATOR + 
+				 HostDiscovery.thisHost.getLastPing() + NetworkSerializable.VARIABLE_MEMBER_SEPARATOR +				
+				 startingTime + NetworkSerializable.VARIABLE_MEMBER_SEPARATOR +
+				 action + NetworkSerializable.VARIABLE_MEMBER_SEPARATOR +				 
+				 position + NetworkSerializable.VARIABLE_END_OF_VARIABLES);
+	}
+
+	public Object networkDeserialize(String data) {
+		// recupero valores
+		String cadenas[] = data.split(""+NetworkSerializable.VARIABLE_MEMBER_SEPARATOR);
+			
+		// creo la instancia Data y la devuelvo
+		Data appData = new Data();
+		Host host = new Host(cadenas[0], cadenas[1]=="true");
+		appData.sourceHost = host;
+		host.setLastPing(Long.parseLong(cadenas[2]));
+		appData.startingTime = Long.parseLong(cadenas[3]);
+		appData.action = Integer.parseInt(cadenas[4]);
+		appData.position = Integer.parseInt(cadenas[5]);
+		return appData;
+	}
 	
 }
